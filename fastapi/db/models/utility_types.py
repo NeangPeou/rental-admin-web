2ecd546ce5a34e36a54b114518ee94d8681f7da2
neangpeou <neangpov66666@gmail.com>
2025-11-24 15:00:02 +0700

    commit

--- fastapi/db/models/utility_types.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import TIMESTAMP, Column, Integer, String, func
-from db.session import Base
-
-class UtilityType(Base):
-    __tablename__ = "t_utility_types"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False, unique=True)
-    created_at = Column(TIMESTAMP, server_default=func.now())
-    updated_at = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
